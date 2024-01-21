7013d9a3d23b3b003537be5907967be0c9379889
Bakul Takthar <bakul184bsingh@gmail.com>
2024-01-21 21:06:29 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import psutil
+from plyer import notification
+
+frequency = int(input("how often should the notifications be sent?"))
+battery = psutil.sensors_battery()
+print(battery)
+
+percent = battery.percent
+
+while True:
+    battery = psutil.sensors_battery()
+    current_percent = battery.percent
+    change = current_percent - percent
+    diff = abs(change)
+
+    if diff >= frequency:
+        notification.notify(
+            title = "Current Battery Percentage",
+            message = f"{str(current_percent)}% battery remaining ",
+            timeout = 5
+        )
+
+        percent = current_percent
+
+    continue
